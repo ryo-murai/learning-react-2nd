fc7578ec8a22099c74a9a0a10205d5492728aa18
ryo-murai <murai.ry@gmail.com>
2025-05-06 23:11:38 +0900

    add source-map

--- chap5/recipes-app/webpack.config.js
+++ chap5/recipes-app/webpack.config.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: "source-map",
   output: {
     path: path.join(__dirname, "dist", "assets"),
     filename: "bundle.js"
