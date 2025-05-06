2f94985148a14f9225c0881642be63f863958d22
ryo-murai <murai.ry@gmail.com>
2025-05-06 23:05:39 +0900

    fix to use createRoot instead of render for React18

--- chap5/recipes-app/src/index.js
+++ chap5/recipes-app/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import data from "../data/recipes.json";
 import Menu from "./components/menu";
 
-render(
-  <Menu recipes={data} />,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Menu recipes={data} />
 );
