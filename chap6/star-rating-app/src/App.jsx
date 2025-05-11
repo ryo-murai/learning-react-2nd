cf3642f3ef12eecde237c3be10b4605633090ab6
ryo-murai <murai.ry@gmail.com>
2025-05-12 00:09:37 +0900

    add color list

--- chap6/star-rating-app/src/App.jsx
+++ chap6/star-rating-app/src/App.jsx
@@ -3,8 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import StarRating from './components/StarRating'
+import colorData from './data/color-data.json'
+import ColorList from './components/ColorList'
+import Color from './components/Color'
 
 function App() {
+  const [colors] = useState(colorData)
   const [count, setCount] = useState(0)
 
   return (
@@ -26,10 +30,11 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <StarRating
-          style={{backgroundColor: "lightblue"}}
+          style={{ backgroundColor: "lightblue" }}
           onDoubleClick={e => alert("Star rating double clicked!")}
         />
       </div>
+      <ColorList colors={colors} />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- chap6/star-rating-app/src/components/StarRating.jsx
+++ chap6/star-rating-app/src/components/StarRating.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaStar } from "react-icons/fa";
 
 const Star = ({ selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-export default function StarRating({style= {}, totalStars = 5, ...props}) {
-  const [selectedStars, setSelectedStars] = useState(3);
+export default function StarRating({ style = {}, totalStars = 5, selectedStars = 0, ...props }) {
   return (
     <div style={{ padding: "5px", ...style }} {...props}>
       {[...Array(totalStars)].map((_, i) => (
         <Star
           key={i}
           selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
         />
       ))}
       <p>
