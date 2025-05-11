8ad169d01bad323b311313342dd848b57b064886
ryo-murai <murai.ry@gmail.com>
2025-05-11 22:49:11 +0900

    add stars

--- chap6/star-rating-app/src/App.jsx
+++ chap6/star-rating-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StarRating from './components/StarRating'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,9 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <p>
+          <StarRating />
         </p>
       </div>
       <p className="read-the-docs">
