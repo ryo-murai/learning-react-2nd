02d6bf8338dc807dba8b33aab59ef82c96d5cd65
ryo-murai <murai.ry@gmail.com>
2025-05-14 22:34:48 +0900

    add effect to show alert

--- chap7/chap7-app/src/components/Checkbox.jsx
+++ chap7/chap7-app/src/components/Checkbox.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Checkbox() {
   const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    alert(`checked: ${checked}`);
+  });
 
   return (
     <>
