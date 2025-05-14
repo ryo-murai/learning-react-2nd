b52fe912a9b05ea8e225ae6b1877abac1c3cc94f
ryo-murai <murai.ry@gmail.com>
2025-05-14 22:53:33 +0900

    add some effects

--- chap7/chap7-app/src/App.jsx
+++ chap7/chap7-app/src/App.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Checkbox from './components/Checkbox'
+import { use } from 'react';
 
 function App() {
+  const [val, set] = useState("");
+  const [phrase, setPhrase] = useState("Hello World");
+
+  const createPhrase = () => {
+    setPhrase(val);
+    set("");
+  }
+
+  useEffect(() => {
+    console.log("App mounted");
+  }, []);
+
+  useEffect(() => {
+    console.log(`typing "${val}"`);
+  }, [val]);
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`);
+  }, [phrase]);
 
   return (
     <>
@@ -16,7 +37,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Checkbox />
+      <div>
+        {/* <Checkbox /> */}
+      </div>
+      <div>
+        <label>Favorite phrase:</label> <input
+          value={val}
+          placeholder={phrase}
+          onChange={e => set(e.target.value)}
+        />
+        <button onClick={createPhrase}>send</button>
+      </div>
+
     </>
   )
 }
