fdbec7612709ec8aec5e9a6983e15f5d9928e2c1
ryo-murai <murai.ry@gmail.com>
2025-05-25 23:06:11 +0900

    add cache to localStorage

--- chap8/chap8-app/src/components/GitHubUser.jsx
+++ chap8/chap8-app/src/components/GitHubUser.jsx
@@ -1,10 +1,28 @@
 import { useState, useEffect } from 'react';
 
+const storage = localStorage;
+
+const loadJSON = key => key && JSON.parse(storage.getItem(key));
+
+const saveJSON = (key, value) => storage.setItem(key, JSON.stringify(value));
+
 export default function GitHubUser({ login }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(loadJSON(`github-user-${login}`));
 
   useEffect(() => {
-    if (login) {
+    if (user) {
+      const { name, avatar_url, location } = user;
+      saveJSON(`github-user-${login}`, {
+        name,
+        login,
+        avatar_url,
+        location
+      });
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (login && !user || user.login !== login) {
       fetch(`https://api.github.com/users/${login}`)
         .then(response => response.json())
         .then(setUser)
