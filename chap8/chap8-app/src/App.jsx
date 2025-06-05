cc4fe881f7521a3fa9a92462f8c0b1d20d151133
ryo-murai <murai.ry@gmail.com>
2025-06-05 22:57:11 +0900

    add List with render props

--- chap8/chap8-app/src/App.jsx
+++ chap8/chap8-app/src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
 import FetchApi from './components/FetchApi'
 import GitHubUser, { GitHubUserCache } from './components/GitHubUser'
+import List from './components/List'
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 }
+]
 
 function App() {
 
@@ -9,6 +17,12 @@ function App() {
       <FetchApi />
       <GitHubUserCache login="moonhighway" />
       <GitHubUser login="moonhighway" />
+
+      <List
+        data={tahoe_peaks}
+        renderItem={item => `${item.name} (${item.elevation} ft)`}
+        renderEmpty={<p>No peaks found.</p>}
+      />
     </>
   )
 }
