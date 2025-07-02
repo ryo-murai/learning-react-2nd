7d2d9797506d5a950b33ffe84e7f95d978ef0f79
ryo-murai <murai.ry@gmail.com>
2025-07-02 23:02:40 +0900

    add status throwing promise, but doesn't work

--- chap9/chap9-app/src/App.jsx
+++ chap9/chap9-app/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState, Suspense, lazy } from 'react'
 import './App.css'
 
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
+import GridLoader from 'react-spinners/GridLoader'
+
 import Agreement from './components/Agreement'
 //import AppInternal from './AppInternal'
-import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
 import ErrorBoundary from './components/ErrorBoundary';
+import Status from './components/Status';
 
 const AppInternal = lazy(() => import('./AppInternal'));
+
 
 function App() {
   const [agree, setAgree] = useState(false)
@@ -15,11 +19,18 @@ function App() {
     return <Agreement onAgree={() => setAgree(true)} />
   } else {
     return (
-      <ErrorBoundary>
-        <Suspense fallback={<ClimbingBoxLoader />} >
-          <AppInternal />
+      <>
+        <ErrorBoundary>
+          <Suspense fallback={<ClimbingBoxLoader />} >
+            <AppInternal />
+          </Suspense>
+        </ErrorBoundary>
+        <Suspense fallback={<GridLoader />}>
+          <ErrorBoundary>
+            <Status />
+          </ErrorBoundary>
         </Suspense>
-      </ErrorBoundary>
+      </>
     )
   }
 }
